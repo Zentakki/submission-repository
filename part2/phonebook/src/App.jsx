746dbdb5d157fdabbd3689a1df412a1a29d8ed8a
Zentakki <likorkaffe@gmail.com>
2025-08-30 10:15:28 +0200

    part 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,35 @@
 import { useState } from 'react'
 
-const Person = ({ name }) => {
-  return <li>{name}</li>
+const Person = ({ name, phoneNumber }) => {
+  return <li>{name} {phoneNumber}</li>
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      phoneNumber: '666-666-666'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
 
   const addNewPerson = (event) => {
     event.preventDefault()
-    const newPersonObject = { name: newName }
+    const newPersonObject = {
+      name: newName,
+      phoneNumber: newPhoneNumber
+    }
 
-    if (persons.some( person => person.name === newName)) {
+    if (
+      persons.some( person => person.name === newName)
+    ) {
+      setNewName('')
       return (alert(`${newName} already in the phonebook`))
     } else {
       setPersons(persons.concat(newPersonObject))
       setNewName('')
+      setNewPhoneNumber('')
     }
   }
 
@@ -26,12 +37,19 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNewPhoneNumber = (event) => {
+    setNewPhoneNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewPerson}>
         <div>
-          name: <input value={newName} onChange={handleNewName} />
+          name: <input value={newName} onChange={handleNewName} required />
+        </div>
+        <div>
+          phone number: <input value={newPhoneNumber} onChange={handleNewPhoneNumber} required />
         </div>
         <div>
           <button type="submit">add</button>
@@ -40,7 +58,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-            <Person key={person.name} name={person.name}/>
+            <Person key={person.name} name={person.name} phoneNumber={person.phoneNumber}/>
           ))}
       </ul>
     </div>
